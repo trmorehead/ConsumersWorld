78c3f42c76352637f2e0b5e2f0132edffd94d2d1
tmorehead <tmorehead@outlook.com>
2015-11-08 19:42:18 -0500

    removed appCompat and commented out two methods

--- app/src/main/java/segroup/se/consumersworld/MainActivity.java
+++ app/src/main/java/segroup/se/consumersworld/MainActivity.java
@@ -1,12 +1,9 @@
 package segroup.se.consumersworld;
 
+import android.app.Activity;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.TextView;
@@ -15,7 +12,7 @@ import android.widget.Toast;
 //first commit the changes to git then push the file to github.
 //then others can sync and update their files without even closing AS.
 
-public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+public class MainActivity extends Activity implements AdapterView.OnItemSelectedListener {
 
     Spinner spinner;
 
@@ -31,27 +28,27 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         spinner.setOnItemSelectedListener(this);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
+    //@Override
+    //public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+       // getMenuInflater().inflate(R.menu.menu_main, menu);
+       // return true;
+   // }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
+   // @Override
+   // public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+      //  int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+       // if (id == R.id.action_settings) {
+         //   return true;
+       // }
 
-        return super.onOptionsItemSelected(item);
-    }
+       // return super.onOptionsItemSelected(item);
+   // }
 
 
     @Override
