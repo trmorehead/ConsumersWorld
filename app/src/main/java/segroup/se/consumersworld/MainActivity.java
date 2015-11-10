f06c04f2a19027b0f5ee1da3bbd464ed932d3347
tmorehead <tmorehead@outlook.com>
2015-11-10 10:42:02 -0500

    added button functionality and a button to screen 1. Also changed spinner text color

--- app/src/main/java/segroup/se/consumersworld/MainActivity.java
+++ app/src/main/java/segroup/se/consumersworld/MainActivity.java
@@ -1,10 +1,12 @@
 package segroup.se.consumersworld;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -14,12 +16,16 @@ import android.widget.Toast;
 
 public class MainActivity extends Activity implements AdapterView.OnItemSelectedListener {
 
+    private Button btnClick;
     Spinner spinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnClick = (Button) findViewById(R.id.Button);
+
 
         spinner = (Spinner)findViewById(R.id.spinner);
 
@@ -54,6 +60,7 @@ public class MainActivity extends Activity implements AdapterView.OnItemSelected
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
         TextView myText = (TextView) view;
+        ((TextView) parent.getChildAt(0)).setTextColor(Color.WHITE);
         Toast.makeText(this, "You Selected " + myText.getText(), Toast.LENGTH_SHORT).show();
     }
 
