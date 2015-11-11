ebf13e2e2f75793479106bc1558e2974cd43d1b6
Thomas Morehead <trmorehead0@frostburg.edu>
2015-11-10 21:19:56 -0500

    added background welcome

--- app/src/main/java/segroup/se/consumersworld/Page2Activity.java
+++ app/src/main/java/segroup/se/consumersworld/Page2Activity.java
@@ -19,14 +19,15 @@ public class Page2Activity extends Activity implements AdapterView.OnItemSelecte
 
     Spinner spinner;
     Spinner spinner2;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_page2);
-       // Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        // Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         //setSupportActionBar(toolbar);
 
-        spinner = (Spinner)findViewById(R.id.spinner);
+        spinner = (Spinner) findViewById(R.id.spinner);
         spinner2 = (Spinner) findViewById(R.id.spinner2);
         ArrayAdapter adapter = ArrayAdapter.createFromResource(this, R.array.Currencies, android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
@@ -34,23 +35,20 @@ public class Page2Activity extends Activity implements AdapterView.OnItemSelecte
         spinner2.setAdapter(adapter);
         spinner2.setOnItemSelectedListener(this);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+    }
+
+    public void onClick(View view) {
+        Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show();
     }
 
 
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
 
-            TextView myText = (TextView) view;
-            ((TextView) parent.getChildAt(0)).setTextColor(Color.WHITE);
-            Toast.makeText(this, "You Selected " + myText.getText(), Toast.LENGTH_SHORT).show();
+        TextView myText = (TextView) view;
+        ((TextView) parent.getChildAt(0)).setTextColor(Color.WHITE);
+        Toast.makeText(this, "You Selected " + myText.getText(), Toast.LENGTH_SHORT).show();
 
     }
 
@@ -58,4 +56,5 @@ public class Page2Activity extends Activity implements AdapterView.OnItemSelecte
     public void onNothingSelected(AdapterView<?> parent) {
 
     }
+
 }
