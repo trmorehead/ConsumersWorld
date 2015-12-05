f423f3fdca8730c1dd88c8d70e9554eb07eeb735
Thomas Morehead <trmorehead0@frostburg.edu>
2015-12-05 16:26:07 -0500

    added Fetch.java class and Calculator.java with empty methods for now

---
+++ app/src/main/java/segroup/se/consumersworld/Calculator.java
@@ -0,0 +1,76 @@
+package segroup.se.consumersworld;
+
+import java.util.HashMap;
+
+/**
+ * Calculates the
+ * Created by tmore on 12/5/2015.
+ */
+public class Calculator {
+    private String userStartCurrency;
+    private String userTargetCurrency;
+    private double userStartAmount;
+    private double resultToUser;
+    private HashMap currencyHashMap;
+
+    /**
+     *
+     */
+    public Calculator(){
+
+    }
+
+    /**
+     *Creates a Hashmap from the text file
+     * that is created using the Fetch class.
+     */
+    public void altCurrencyValue(){
+
+    }
+
+    /**
+     * Retrieves a double value of world currency values
+     * that are equal to 1 U.S. dollar from a Hashmap.
+     * These values are  to be used in the convertCurrency method
+     * @return double
+     */
+    public double getStartCurrencyValue()
+    {
+        return .2;
+    }
+
+    /**
+     * Retrieves a double value of world currency values
+     * that are equal to 1 U.S. dollar from a Hashmap.
+     * These values are to be used in the convertCurrency method.
+     * @return double
+     */
+    public double getAltCurrencyValue()
+    {
+        return .2;
+    }
+
+    /**
+     * Uses a mathematical equation along with a Switch Case
+     * to convert the value of the  UserStartCurrency to an equal
+     * value of the UserTargetCurrency .
+     * @return double
+     */
+    public double convertCurrency()
+    {
+        return .2;
+    }
+
+    /**
+     * Converts the double value of convertCurrency method
+     * to a string type that will be displayed in the
+     * UserTargetCurrency textbox.
+     * @return String
+     */
+    public String toString()
+    {
+        return "to string";
+    }
+
+
+}
---
+++ app/src/main/java/segroup/se/consumersworld/Fetch.java
@@ -0,0 +1,52 @@
+package segroup.se.consumersworld;
+
+/**
+ * Fetch class will fetch a JSON file from www.openexchangerates.org.
+ * The resulting JSON file will be parsed using the GSON library.
+ * Once parsed the 10 currencies will be written to a local text file.
+ * Created by tmore on 12/5/2015.
+ */
+public class Fetch {
+    Exception Exception;
+
+    /**
+     * constructor
+     */
+    public Fetch()
+    {
+
+    }
+
+    /**
+     *
+     */
+    public void onPreExecute()
+    {
+
+    }
+
+    /**
+     *
+     * @return String
+     */
+    public String doInBackground()
+    {
+        return "String";
+    }
+
+    /**
+     *
+     */
+    public void onProgressUpdate()
+    {
+
+    }
+
+    /**
+     *
+     */
+    public void onPostExecute()
+    {
+
+    }
+}
